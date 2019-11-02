34f21dcb956aaae032c18069c2bacf70d9bf502b
bensterenson <bensterenson@mail.tau.ac.il>
2019-11-02 23:19:54 +0200

    support k8s for collection-api

--- collection_ms/deploy.sh
+++ collection_ms/deploy.sh
@@ -6,4 +6,4 @@ docker container ls -a
 
 docker stop $CONTAINER_NAME
 
-docker run --rm -d --name $CONTAINER_NAME -p 80:80 $CONTAINER_NAME
+docker run --rm --name $CONTAINER_NAME -p 80:80 $CONTAINER_NAME
